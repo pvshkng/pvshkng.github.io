a61b59190791dc947bce2f79aeb44926d6c7bc0a
Puvish Kungsapattarakul <osacn_puvish.kungsapattarakul@krungthai.com>
2023-12-26 17:49:08 +0700

    added chatbox

---
+++ src/assets/js/chat.js
@@ -0,0 +1,65 @@
+function openChat() {
+  const chatbox = document.getElementById("overall-chat-box");
+  const button = document.getElementById("chat-open-button");
+  chatbox.style.display = "block";
+
+  setTimeout(() => {
+    chatbox.classList.add("chatbox-activated");
+    button.style.display = "none";
+  }, 100);
+}
+
+function closeChat() {
+  const chatbox = document.getElementById("overall-chat-box");
+  const button = document.getElementById("chat-open-button");
+  chatbox.classList.remove("chatbox-activated");
+
+  setTimeout(() => {
+    chatbox.style.display = "none";
+    button.style.display = "block";
+  }, 100);
+}
+
+var input = document.getElementById("userInput");
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    //event.preventDefault();
+    //document.getElementById("sendIcon").click();
+    handleSend();
+  }
+});
+
+function handleSend() {
+  const userInput = document.getElementById("userInput");
+
+  if (!userInput.value) {
+    return;
+  } else {
+    const tempText = userInput.value;
+    userInput.value = "";
+
+    //User Text
+    /* const p = document.createElement("p");
+    const span = document.createElement("span");
+    p.classList.add("text-bubble", "user");
+    span.classList.add("text-cont", "user");
+    span.innerText = tempText;
+    p.appendChild(span); */
+    insertBubble("user", tempText);
+
+    //Loading Text
+    insertBubble("bot", '', true)
+  }
+}
+
+function insertBubble(who, text, isLoading) {
+  const p = document.createElement("p");
+  const span = document.createElement("span");
+  p.classList.add("text-bubble", who);
+  span.classList.add("text-cont", who);
+  span.innerText = text;
+  p.appendChild(span);
+
+  const msgArea = document.getElementById("msgArea");
+  msgArea.appendChild(p);
+}
